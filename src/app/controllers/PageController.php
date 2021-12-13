45a67c7291193c7ac9a67e704c59cb635be05ca9
KageHitokiri <skhcri@gmail.com>
2021-12-13 09:43:49 +0100

    novedades implementado

--- src/app/controllers/PageController.php
+++ src/app/controllers/PageController.php
@@ -19,7 +19,8 @@ class PageController
         $repositorio = new ProductRepository;
         $carrusel = $repositorio->getCarrusel();
         $destacados = $repositorio->getDestacados();
+        $novedades = $repositorio->getNovedades();
 
-        return $this->container->renderer->render($response, "index.view.php", compact('title', 'carrusel','destacados'));
+        return $this->container->renderer->render($response, "index.view.php", compact('title', 'carrusel','destacados', 'novedades'));
     }
 }
